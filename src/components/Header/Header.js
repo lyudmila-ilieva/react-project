c68889877471228ae7256b49e78297f8abb06f77
lyudmila-ilieva <79860332+lyudmila-ilieva@users.noreply.github.com>
2021-03-27 14:41:29 +0200

    Add SignOut and personal greeting

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,59 @@
 import './Header.css';
+import React, { useContext } from 'react';
 import { Link, NavLink } from 'react-router-dom';
-// import firebase from "../../firebase";
+import { AuthContext } from "../../Auth";
+import firebase from "../../firebase";
 
 function Header() {
 
-  // const logOut = () => {
-  //   firebase.auth().signOut();
-  // };
+  const { currentUser } = useContext(AuthContext);
 
+  const logOut = () => {
+    firebase.auth().signOut();
+  };
+
+  if(currentUser) {
+    return (
+      <>
+        <nav className="navbar">
+          <div className="navbar-container">
+            <Link to="/" className="navbar-logo">
+              CASA de AURORA
+            </Link>
+          </div>
+          
+          <ul className="nav-menu">
+            {/* <li className="nav-item">
+            <p className="nav-greeting">Welcome, {currentUser.email}</p>
+            </li> */}
+            <li className="nav-item">
+              <NavLink activeStyle={{fontSize: '1.1em'}} exact={true} to="/about" className="nav-links">
+                About us                  
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink activeStyle={{fontSize: '1.1em'}} exact={true} to="/contacts" className="nav-links">
+                Contacts                  
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink activeStyle={{fontSize: '1.1em'}} exact={true} to="/account" className="nav-links">
+                My Account                  
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink activeStyle={{fontSize: '1.1em'}} exact={true} onClick={logOut} to="/sign-in" className="nav-links">
+                Sign Out                 
+              </NavLink>
+            </li>
+            <li className="nav-item">
+            <p className="nav-greeting">Welcome, {currentUser.email}</p>
+            </li>
+          </ul>
+        </nav>            
+      </>
+    )
+  } else if(!currentUser) {
     return (
         <>
           <nav className="navbar">
@@ -18,34 +64,30 @@ function Header() {
             </div>
             <ul className="nav-menu">
               <li className="nav-item">
-                <NavLink activeStyle={{fontSize: '1.3em'}} exact={true} to="/about" className="nav-links">
+                <NavLink activeStyle={{fontSize: '1.1em'}} exact={true} to="/about" className="nav-links">
                   About us                  
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink activeStyle={{fontSize: '1.3em'}} exact={true} to="/contacts" className="nav-links">
+                <NavLink activeStyle={{fontSize: '1.1em'}} exact={true} to="/contacts" className="nav-links">
                   Contacts                  
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink activeStyle={{fontSize: '1.3em'}} exact={true} to="/sign-up" className="nav-links">
+                <NavLink activeStyle={{fontSize: '1.1em'}} exact={true} to="/sign-up" className="nav-links">
                   Register
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink activeStyle={{fontSize: '1.3em'}} exact={true} to="/sign-in" className="nav-links">
+                <NavLink activeStyle={{fontSize: '1.1em'}} exact={true} to="/sign-in" className="nav-links">
                   Sign In                  
                 </NavLink>
               </li>
-              {/* <li className="nav-item">
-                <Link activeStyle={{fontSize: '1.3em'}} exact={true} onClick={logOut} className="nav-links">
-                  Sign Out                  
-                </Link>
-              </li> */}
             </ul>
           </nav>            
         </>
     )
+  }
 }
 
 export default Header;
